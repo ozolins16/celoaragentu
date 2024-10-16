cf81dd610037082d84993d29292ed83efa79cdeb
ozolins16 <linardsozolins16@gmail.com>
2024-10-17 00:44:23 +0300

    test4

--- api/fetch-hotels.js
+++ api/fetch-hotels.js
@@ -5,10 +5,10 @@ export default async function handler(req, res) {
 
   try {
     // Fetch the hotel data from the external API
-    const response = await fetch('https://pim.novatours.eu/webservice/celo111/LV/list-hotels', {
+    const response = await fetch('https://pim.novatours.eu/webservice/celo111/list-hotels', {
       method: 'GET',
       headers: {
-        'Authorization': 'Bearer your_token_here',
+        'Authorization': 'Bearer 72ae9d228c3f630b446a1b8a8cb8cbf3',
         'User-Agent': 'Mozilla/5.0'
       }
     });
@@ -19,8 +19,8 @@ export default async function handler(req, res) {
 
     const data = await response.json();
 
-    console.log(JSON.stringify(data, null, 2));
 
+    console.log(JSON.stringify(data, null, 2));    
     // Filter the data based on the location provided
     // const filteredData = data.hotels.filter(hotel => hotel.country_code === location);
 
@@ -31,3 +31,12 @@ export default async function handler(req, res) {
     res.status(500).json({ message: 'Error fetching hotel data', error: error.message });
   }
 }
+
+
+
+
+
+
+// Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.5938.132 Safari/537.36
+// https://pim.novatours.eu/webservice/celo111/LV/list-hotels?country_code[]=LV
+// 72ae9d228c3f630b446a1b8a8cb8cbf3
