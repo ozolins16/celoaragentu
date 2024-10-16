8e33fa6cfa2a636e250266d82a24c9cf49207e72
ozolins16 <linardsozolins16@gmail.com>
2024-10-17 00:00:31 +0300

    test

--- api/fetch-hotels.js
+++ api/fetch-hotels.js
@@ -4,7 +4,7 @@ export default async function handler(req, res) {
   try {
     const response = await axios.get('https://pim.novatours.eu/webservice/celo111/LV/list-hotels?country_code[]=LV', {
       headers: {
-        'Authorization': 'Bearer // 72ae9d228c3f630b446a1b8a8cb8cbf3',  // Replace with your actual token
+        'Authorization': 'Bearer 72ae9d228c3f630b446a1b8a8cb8cbf3',  // Replace with your actual token
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.5938.132 Safari/537.36'
       }
     });
