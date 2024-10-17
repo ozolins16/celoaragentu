d2e1b0eb7bbdac24de3047781bdb82535e47ed78
ozolins16 <linardsozolins16@gmail.com>
2024-10-17 23:35:12 +0300

    test

--- api/fetch-destinations.js
+++ api/fetch-destinations.js
@@ -1,7 +1,18 @@
+const output = document.getElementById('output');
 
+async function destinationData() {
+    const response = await fetch('https://pim.novatours.eu/webservice/celo111/LV/list-destinations-tab', {
+              method: 'GET',
+              headers: {
+                'Authorization': 'Bearer 72ae9d228c3f630b446a1b8a8cb8cbf3',
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.5938.132 Safari/537.36',
+              },
+            });
+    const data = response.json();
+    output.textContent(data);
+}
 
-
-
+destinationData();
 
 
 
@@ -36,24 +47,8 @@
 //   }
 // }
 
+// module.exports = handler;
 
-
-
-
-// document.getElementById('search').addEventListener('click', () => {
-//   fetch('/api/fetch-hotels')
-//     .then(response => {
-//       console.log('Status:', response.status); // Log the status code
-//       console.log('Status Text:', response.statusText); // Log status text
-//       if (!response.ok) {
-//         throw new Error(`Network response was not ok: ${response.statusText} (Status: ${response.status})`);
-//       }
-//       return response.json();
-//     })
-//     .then(data => {
-//       document.getElementById('output').textContent = JSON.stringify(data, null, 2);
-//     })
-//     .catch(error => {
-//       document.getElementById('output').textContent = 'Error fetching data: ' + error.message;
-//     });
-// });
+// Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.5938.132 Safari/537.36
+// https://pim.novatours.eu/webservice/celo111/LV/list-hotels?country_code[]=LV
+// 72ae9d228c3f630b446a1b8a8cb8cbf3
